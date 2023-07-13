78bbb053b8edabe39d0c8f3a8177ebc7f78c91a6
makisacik <glsabaton@outlook.com>
2023-07-13 21:46:29 +0300

    Initial Commit

---
+++ UberCloneSwiftUI/UberCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneSwiftUIApp.swift
+//  UberCloneSwiftUI
+//
+//  Created by Mehmet Ali Kısacık on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
