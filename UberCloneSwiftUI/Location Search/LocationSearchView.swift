1ce066e4105761ec1d45fb65e03adf5959adbe2a
makisacik <glsabaton@outlook.com>
2023-07-14 01:56:17 +0300

    Create location search view

---
+++ UberCloneSwiftUI/Location Search/LocationSearchView.swift
@@ -0,0 +1,50 @@
+//
+//  LocationSearchView.swift
+//  UberCloneSwiftUI
+//
+//  Created by Mehmet Ali Kısacık on 14.07.2023.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    @State private var startLocationText = ""
+    @State private var destinationLocationText = ""
+
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray))
+                        .frame(width: 6, height: 6)
+                    Rectangle()
+                        .fill(Color(.systemGray))
+                        .frame(width: 1, height: 24)
+                    Rectangle()
+                        .fill(Color(.systemGray))
+                        .frame(width: 6, height: 6)
+                }
+                
+                VStack {
+                    TextField("Current Location", text: $startLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                    
+                    TextField("Current Location", text: $destinationLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGray4))
+                }
+                
+            }
+            .padding(.horizontal)
+            
+        }
+    }
+}
+
+struct LocationSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchView()
+    }
+}
