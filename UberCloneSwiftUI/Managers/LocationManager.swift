faee81f36658e2a2ebfa7ff5602e6e77c58c5879
makisacik <glsabaton@outlook.com>
2023-09-27 13:47:55 +0100

    Set region on user location update

--- UberCloneSwiftUI/Managers/LocationManager.swift
+++ UberCloneSwiftUI/Managers/LocationManager.swift
@@ -9,6 +9,7 @@ import CoreLocation
 
 final class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
+    var isFirstLocationSet = false
     
     override init() {
         super.init() 
